bedb0c283e143afd7897d6e1cd6ba1e9dda57b44
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-08-27 18:34:30 +0500

    image fixed

--- src/re-usible/Search/index.tsx
+++ src/re-usible/Search/index.tsx
@@ -4,7 +4,7 @@ const Search = () => {
 
     return (
         <div className="search-box">
-            <button className="btn-search"><Image className="search-icon"  src={SearchImage.src} alt="search"/></button>
+            <button className="btn-search"><Image className="search-icon"  src={SearchImage.src} alt="search" width={22} height={22}/></button>
             <input type="text" className="input-search" placeholder="Type to Search..." />
         </div>
     )
