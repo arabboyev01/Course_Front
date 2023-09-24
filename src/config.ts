904864f80d2f3ba4c5e58cfbc4e73425323525e4
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-09-24 23:28:05 +0500

    url changes

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 import { ApiService } from '@/pages/api'
-export const api = new ApiService("http://13.49.75.142:3002");
+// export const api = new ApiService("http://13.49.75.142:3002");
+// export const api = new ApiService('https://13.49.75.142:8080');
 
-// export const api = new ApiService('http://localhost:3002')
+
+export const api = new ApiService('http://13.51.159.90:8080')
